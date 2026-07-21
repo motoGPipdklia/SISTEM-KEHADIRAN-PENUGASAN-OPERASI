ac5f3035cfc2bbdff916c0fc3177dff4b8ab7c8b
motoGPipdklia <ipdkliamotogp@gmail.com>
2026-07-21 13:48:21 +0800

    Delete js/supabase-client.js

--- js/supabase-client.js
+++
@@ -1,8 +0,0 @@
-// SKPO Supabase Client
-
-window.supabaseClient = window.supabase.createClient(
-    window.SKPO_CONFIG.SUPABASE_URL,
-    window.SKPO_CONFIG.SUPABASE_ANON_KEY
-);
-
-console.log("✅ Supabase Connected");
